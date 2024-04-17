2cf84155a351d3c34acad875932cda417588da1a
Ron Pasillas <ron@alley.com>
2024-04-17 10:50:59 -0700

    #14 PHPCS Fixes and text domain translation.

--- src/class-endpoint-get-conditions.php
+++ src/class-endpoint-get-conditions.php
@@ -13,6 +13,9 @@ namespace Alley\WP\WP_Conditional_Blocks;
  * This class is responsible for registering a REST API endpoint and defining its functionality.
  */
 class Endpoint_Get_Conditions {
+	/**
+	 * Constructor
+	 */
 	public function __construct() {
 		add_action( 'rest_api_init', [ $this, 'register_route' ] );
 	}
@@ -23,11 +26,15 @@ class Endpoint_Get_Conditions {
 	 * @return void
 	 */
 	public function register_route(): void {
-		register_rest_route('conditional-blocks/v1', '/get-conditions/', [
-			'methods'  => 'GET',
-			'callback' => [ $this, 'get_response' ],
-			'permission_callback' => '__return_true',
-		] );
+		register_rest_route(
+			'conditional-blocks/v1',
+			'/get-conditions/',
+			[
+				'methods'             => 'GET',
+				'callback'            => [ $this, 'get_response' ],
+				'permission_callback' => '__return_true',
+			]
+		);
 	}
 
 	/**
@@ -38,9 +45,12 @@ class Endpoint_Get_Conditions {
 	public function get_response(): \WP_REST_Response {
 		$conditions = Conditions::get_instance();
 
-		return new \WP_REST_Response( [
-			'message' => !empty( $conditions->get_conditions() ) ? $this->format_conditions( $conditions->get_conditions() ) : 'No conditions found.'
-		], 200 );
+		return new \WP_REST_Response(
+			[
+				'message' => ! empty( $conditions->get_conditions() ) ? $this->format_conditions( $conditions->get_conditions() ) : __( 'No conditions found.', 'wp-conditional-blocks' ),
+			],
+			200
+		);
 	}
 
 	/**
@@ -48,13 +58,15 @@ class Endpoint_Get_Conditions {
 	 *
 	 * @param array{int, array{name:string, slug:string, callable:callable}} $conditions The conditions to be formatted.
 	 *
-	 *
 	 * @return array{array{name:string, slug:string}} formatted conditions.
 	 */
 	private function format_conditions( array $conditions ): array {
-		return array_map( function( $item ) {
-			unset( $item['callable'] );
-			return $item;
-		}, $conditions );
+		return array_map(
+			function ( $item ) {
+				unset( $item['callable'] );
+				return $item;
+			},
+			$conditions
+		);
 	}
 }
