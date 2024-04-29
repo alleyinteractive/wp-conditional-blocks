efddf72ddde73d3d4e72b44679328aa72891bb93
Travis Weston <anubisthejackle@users.noreply.github.com>
2024-04-29 11:10:12 -0400

    Add a helpers file and abstract out the pre_render_block callback for is-true and is-false

--- blocks/is-false/index.php
+++ blocks/is-false/index.php
@@ -5,6 +5,11 @@
  * @package wp-conditional-blocks
  */
 
+use function Alley\WP\WP_Conditional_Blocks\is_active_block;
+use function Alley\WP\WP_Conditional_Blocks\is_active_parent_block;
+use function Alley\WP\WP_Conditional_Blocks\on_bool;
+use function Alley\WP\WP_Conditional_Blocks\pre_render_on_bool;
+
 /**
  * Registers the block using the metadata loaded from the `block.json` file.
  * Behind the scenes, it registers also all assets so they can be enqueued
@@ -27,36 +32,10 @@ add_action( 'init', 'wp_conditional_blocks_is_false_block_init' );
  * Short-circuit the display of blocks inside if the outer condition isn't false.
  *
  * @param string|null   $pre_render   The pre-rendered content. Default null.
- * @param WP_Block      $parsed_block The block being rendered.
+ * @param array         $parsed_block The block being rendered.
  * @param WP_Block|null $parent_block If this is a nested block, a reference to the parent block.
  */
 function wp_conditional_blocks_pre_render_is_false_block( $pre_render, $parsed_block, $parent_block ): string|null {
-	/*
-	 * Previously, the condition block added 'conditionResult' as context to this block. However,
-	 * limitations in the context API meant that the context didn't get passed to child blocks when
-	 * the condition block was itself a child block. We now pass the condition block back to a
-	 * separate function that lives outside the context API and evaluates the result.
-	 */
-	if (
-		isset( $parsed_block['blockName'] )
-		&& 'wp-conditional-blocks/is-false' === $parsed_block['blockName']
-		&& $parent_block instanceof WP_Block
-		&& isset( $parent_block->parsed_block['blockName'] )
-		&& 'wp-conditional-blocks/condition' === $parent_block->parsed_block['blockName']
-	) {
-		$context = [];
-
-		if ( isset( $parent_block->context['postId'] ) ) {
-			$context['postId'] = $parent_block->context['postId'];
-		}
-
-		$result = wp_conditional_blocks_condition_block_result( $parent_block->parsed_block, $context );
-
-		if ( false !== $result ) {
-			$pre_render = '';
-		}
-	}
-
-	return $pre_render;
+	return pre_render_on_bool( 'wp-conditional-blocks/is-false', false, $pre_render, $parsed_block, $parent_block );
 }
 add_filter( 'pre_render_block', 'wp_conditional_blocks_pre_render_is_false_block', 10, 3 );
---
+++ src/helpers.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Contains helper functions to be used elsewhere in the plugin.
+ *
+ * @package wp-conditional-blocks
+ */
+
+namespace Alley\WP\WP_Conditional_Blocks;
+
+use WP_Block;
+
+/**
+ * Check if the defined block is the active block. Useful in pre_render_block
+ * callbacks.
+ *
+ * @param string     $block_name The block name to look for. Should be the full namespace.
+ * @param array|null $parsed_block The parsed block array, or null.
+ * @return bool
+ */
+function is_active_block( string $block_name, ?array $parsed_block ): bool {
+	if ( ! isset( $parsed_block['blockName'] ) ) {
+		return false;
+	}
+
+	return $block_name === $parsed_block['blockName'];
+}
+
+/**
+ * Check if the defined block is the active parent block. Useful in pre_render_block
+ * callbacks.
+ *
+ * @param string        $block_name The block name to look for. Should be the full namespace.
+ * @param WP_Block|null $parent_block The parent block to check.
+ * @return bool
+ */
+function is_active_parent_block( string $block_name, ?WP_Block $parent_block ): bool {
+	if ( ! ( $parent_block instanceof WP_Block ) ) {
+		return false;
+	}
+
+	return is_active_block( $block_name, $parent_block->parsed_block );
+}
+
+/**
+ * Handles performing a callback when a condition block returns the expected result.
+ *
+ * @param bool     $result The expected result of the evaluation.
+ * @param WP_Block $parent_block The parent block object.
+ * @param callable $callback The callback to execute on true.
+ * @return mixed
+ */
+function on_bool( bool $result, WP_Block $parent_block, callable $callback, ...$args ): void {
+	$context = [];
+
+	if ( isset( $parent_block->context['postId'] ) ) {
+		$context['postId'] = $parent_block->context['postId'];
+	}
+
+	if( $result === wp_conditional_blocks_condition_block_result( $parent_block->parsed_block, $context ) ) {
+		$callback( ...$args );
+	}
+}
+
+/**
+ * A filter used by both the is-true and is-false blocks to filter their render content,
+ * and either show or hide it based on the result.
+ *
+ * @param string        $block_name   The block name to run on.
+ * @param bool          $bool         True or false. The expected value of the evaluation.
+ * @param string|null   $pre_render   The pre-rendered content. Default null.
+ * @param array         $parsed_block The block being rendered.
+ * @param WP_Block|null $parent_block If this is a nested block, a reference to the parent block.
+ */
+function pre_render_on_bool( string $block_name, bool $bool, $pre_render, $parsed_block, $parent_block ): string|null {
+	/*
+	 * Previously, the condition block added 'conditionResult' as context to this block. However,
+	 * limitations in the context API meant that the context didn't get passed to child blocks when
+	 * the condition block was itself a child block. We now pass the condition block back to a
+	 * separate function that lives outside the context API and evaluates the result.
+	 */
+	if (
+		is_active_block( $block_name, $parsed_block )
+		&& is_active_parent_block( 'wp-conditional-blocks/condition', $parent_block )
+	) {
+		$context = [];
+
+		if ( isset( $parent_block->context['postId'] ) ) {
+			$context['postId'] = $parent_block->context['postId'];
+		}
+
+		if( $bool !== wp_conditional_blocks_condition_block_result( $parent_block->parsed_block, $context ) ) {
+			$pre_render = '';
+		}
+	}
+
+	return $pre_render;
+}
